8f179c16da56e87307f74c1cf28675d1e39aea22
Kaveen Kumar <2idli1vada@email.com>
2021-06-06 13:38:35 +0200

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -6,11 +6,21 @@ app = Flask(__name__)
 # additional code
 
 color = "red"
+# color = os.environ.get('APP_COLOR')
+
+"""
+to run with terminal
+$ export APP_COLOR=blue; python web_app
+
+to run with docker
+$ docker run -e APP_COLOR=blue web_app.py
+"""
 
 @app.route("/")
 def main():
   print(color)
   return render_template("index.html", color=color)
+
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port="8080")
