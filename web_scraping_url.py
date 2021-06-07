a9f3a068ad69db75deb3ba4341f5b8979a06eece
Kaveen Kumar <2idli1vada@email.com>
2021-06-07 11:51:36 +0200

    Update web_scraping_url.py

--- web_scraping_url.py
+++ web_scraping_url.py
@@ -3,7 +3,9 @@ import requests
 
 user_currency = input('>'')
 
-
 html_text = requests.get('https://www.about.me/kiuzzaxk').text
 soup = BeautifulSoup(html_text, 'lxml')
 cryptos = soup.find_all('li', class_ = 'crypto')
+                      
+for idx, crypto in enumerate(cryptos):
+  date = crypto.find('span', class_ = 'date_posted').span.text
