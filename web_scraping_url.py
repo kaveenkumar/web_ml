a2c6b1b07747daefe5a8cae170ea4c16cc320891
Kaveen Kumar <2idli1vada@email.com>
2021-06-07 11:52:25 +0200

    Update web_scraping_url.py

--- web_scraping_url.py
+++ web_scraping_url.py
@@ -3,12 +3,13 @@ import requests
 
 user_currency = input('>'')
 
-html_text = requests.get('https://www.about.me/kiuzzaxk').text
-soup = BeautifulSoup(html_text, 'lxml')
-cryptos = soup.find_all('li', class_ = 'crypto')
-                      
-for idx, crypto in enumerate(cryptos):
-  date = crypto.find('span', class_ = 'date_posted').span.text
+def crypto_value():
+  html_text = requests.get('https://www.about.me/kiuzzaxk').text
+  soup = BeautifulSoup(html_text, 'lxml')
+  cryptos = soup.find_all('li', class_ = 'crypto')
+
+  for idx, crypto in enumerate(cryptos):
+    date = crypto.find('span', class_ = 'date_posted').span.text
 
 if __name__ == '__main__':
   while  True:
