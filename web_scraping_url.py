e2cc6360888f360b3bb8101b1f57321fccadf3a1
Kaveen Kumar <2idli1vada@email.com>
2021-06-16 20:58:52 +0200

    add timer for delay

--- web_scraping_url.py
+++ web_scraping_url.py
@@ -4,14 +4,16 @@ import requests
 user_currency = input('>'')
 
 def crypto_value():
-  html_text = requests.get('https://www.about.me/kiuzzaxk').text
-  soup = BeautifulSoup(html_text, 'lxml')
-  cryptos = soup.find_all('li', class_ = 'crypto')
+	html_text = requests.get('https://www.about.me/kiuzzaxk').text
+ 	soup = BeautifulSoup(html_text, 'lxml')
+  	cryptos = soup.find_all('li', class_ = 'crypto')
 
-  for idx, crypto in enumerate(cryptos):
-    date = crypto.find('span', class_ = 'date_posted').span.text
+  	for idx, crypto in enumerate(cryptos):
+    		date = crypto.find('span', class_ = 'date_posted').span.text
 
 if __name__ == '__main__':
-	crypto_value()
-	time_wait = 10
-	print(f'waiting time {time_wait} minutes')
+  	while True:
+		crypto_value()
+		time_wait = 10
+		print(f'waiting time {time_wait} minutes')
+		time.sleep(time_wait * 60)
