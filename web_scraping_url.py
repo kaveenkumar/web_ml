c768c45975d7024da10eb5e2ad38d1ac77c9a9fd
Kaveen Kumar <2idli1vada@email.com>
2021-06-18 08:55:32 +0200

    Update web_scraping_url.py

--- web_scraping_url.py
+++ web_scraping_url.py
@@ -9,8 +9,14 @@ def crypto_value():
 		      
   cryptos = soup.find_all('li', class_ = 'crypto')
 		      
-  	for idx, crypto in enumerate(cryptos):
-    		date = crypto.find('span', class_ = 'date_posted').span.text
+  for idx, crypto in enumerate(cryptos):
+    date = crypto.find('span', class_ = 'date_posted').span.text
+
+    if 'time.today()' in date:
+      crypto_name = crypto.find('h3', class_ = 'crypto'_n).text.replace(' ', '')
+      crypto_price = crypto.find('span', class_ = 'crypto_p').text
+      more_info = crypto.header.h2.a['href']
+		      
 
 if __name__ == '__main__':
   	while True:
