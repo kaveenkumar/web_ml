066d6010067a41474920496fc824730011e54e18
Kaveen Kumar <2idli1vada@email.com>
2021-06-27 09:16:31 +0200

    python import paths

--- import_modules.py
+++ import_modules.py
@@ -1,5 +1,8 @@
 import sys
-sys.path.append('drive:/modules/')
+
+print(sys.path)  # these paths are where python checks for modules
+
+sys.path.append('drive:/modules/')  # add your path
 import myfuncs as f
 
 f.somefunc()
